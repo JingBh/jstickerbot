23c682879ee40764de2825563c8548cb55f8899c
JingBh <jingbohao@yeah.net>
2022-07-12 21:33:43 +0800

    feat: sticker keywords editor for development

--- bot/commands/__init__.py
+++ bot/commands/__init__.py
@@ -1,11 +1,11 @@
 from telegram.ext import Application
 
+from ._error import error_handler
 from ._inline import inline_handler
-from ..config import is_debug
+from ._sticker import sticker_handler, sticker_tagging_handler, sticker_tagging_get_handler
 from .about import about_handler
 from .start import start_handler
-from ._sticker import sticker_handler
-from ._error import error_handler
+from ..config import is_debug
 
 
 def register(application: Application):
@@ -15,6 +15,8 @@ def register(application: Application):
     application.add_handler(inline_handler)
 
     if is_debug:
-        application.add_handler(sticker_handler)
+        application.add_handler(sticker_handler),
+        application.add_handler(sticker_tagging_handler)
+        application.add_handler(sticker_tagging_get_handler)
     else:
         application.add_error_handler(error_handler)
--- bot/commands/_sticker.py
+++ bot/commands/_sticker.py
@@ -1,16 +1,44 @@
+# For debug mode only
+import json
+
 from telegram import Update
-from telegram.ext import MessageHandler
-from telegram.ext.filters import Sticker
+from telegram.ext import CommandHandler, MessageHandler, ContextTypes
+from telegram.ext.filters import Sticker, TEXT, COMMAND, ChatType
 
 
-async def sticker_callback(update: Update, _):
+async def sticker_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
     pack = update.effective_message.sticker.set_name
     pack_url = f'https://t.me/addstickers/{pack}'
     file_id = update.effective_message.sticker.file_unique_id
+
+    context.user_data['editing_sticker'] = file_id
+
     await update.effective_message.reply_html(f'Sticker ID: <code>{file_id}</code>\n'
                                               f'From pack: {pack_url}',
                                               quote=True)
 
 
 # Blocking here to reply messages one by one
-sticker_handler = MessageHandler(Sticker.ALL, sticker_callback)
+sticker_handler = MessageHandler(Sticker.ALL & ChatType.PRIVATE, sticker_callback)
+
+
+async def sticker_tagging_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    stickers = context.user_data.get('editing_stickers', {})
+    sticker_id = context.user_data.get('editing_sticker')
+    if sticker_id is not None:
+        stickers[sticker_id] = [i.strip() for i in update.effective_message.text.split(' ')]
+        del context.user_data['editing_sticker']
+    context.user_data['editing_stickers'] = stickers
+    await update.effective_message.reply_text('OK')
+
+
+sticker_tagging_handler = MessageHandler(TEXT & ~COMMAND & ChatType.PRIVATE, sticker_tagging_callback)
+
+
+async def sticker_tagging_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    stickers = context.user_data.get('editing_stickers', {})
+    code = json.dumps(stickers, indent=2, ensure_ascii=False)
+    await update.effective_message.reply_html(f'<code>{code}</code>')
+
+
+sticker_tagging_get_handler = CommandHandler('edited', sticker_tagging_command, ChatType.PRIVATE)
